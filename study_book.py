7b98554bd95d37572618cea2e56027ffc7dff0fe
Dzyamon <dmitri.olehnovich@gmail.com>
2023-08-15 23:27:52 +0300

    _current_commit

--- study_book.py
+++ study_book.py
@@ -703,3 +703,28 @@
 # NextClass.printer(x, 'class call')
 # print(x.message)
 
+class Super:
+    def method(self):
+        print('in Super.method')
+    def delegate(self):
+        self.action()
+class Inheritor(Super):
+    pass
+class Replacer(Super):
+    def method(self):
+        print('in Replacer.method')
+class Extender(Super):
+    def method(self):
+        print('starting Extender.method')
+        Super.method(self)
+        print('ending Extender.method')
+class Provider(Super):
+    def action(self):
+        print('in Provider.action')
+if __name__ == '__main__':
+    for klass in (Inheritor, Replacer, Extender):
+        print('\n' + klass.__name__ + '...')
+        klass().method()
+    print('\nProvider...')
+    x = Provider()
+    x.delegate()
