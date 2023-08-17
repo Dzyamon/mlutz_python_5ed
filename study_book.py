55a573abe9d19ec2a4862d196d733932deb408aa
Dzyamon <dmitri.olehnovich@gmail.com>
2023-08-18 00:00:06 +0300

    _current_commit

--- study_book.py
+++ study_book.py
@@ -922,3 +922,29 @@
 # print(next(j)) # 1
 # print(next(i)) # 9
 
+# class Iters:
+#     def __init__(self, value):
+#         self.data = value
+#     def __contains__(self, x):
+#         print('contains: ', end='')
+#         return x in self.data
+#     def __iter__(self):
+#         for x in self.data:
+#             yield x
+#             print('next:', end='')
+#     def __getitem__(self, i):
+#         print(f'get:{i} ', end='')
+#         return self.data[i]
+# if __name__ == '__main__':
+#     X = Iters([1, 2, 3, 4, 5])
+#     print(3 in X)
+#     for i in X: print(i, end='|')
+#     print()
+#     print([i ** 2 for i in X])
+#     print(list(map(bin, X)))
+#     I = iter(X)
+#     while True:
+#         try:
+#             print(next(I), end=' @ ')
+#         except StopIteration:
+#             break
