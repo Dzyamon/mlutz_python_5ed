c9da72fa0cd684dc1930f3f1688f3f663b2cd961
dzyamon <dmitri.olehnovich@gmai.com>
2023-09-06 23:10:01 +0300

    _commit

--- study_book.py
+++ study_book.py
@@ -2160,5 +2160,49 @@
 # print(totals)
 
 # import os
-# output = os.popen('dir')
-# print(output.read())
+# # output = os.popen('dir')
+# # print(output.read())
+# cmd = '/bin/ls -l'
+# output = os.popen(cmd, 'r')
+# for line in output:
+#     print(line, end='')
+
+from tkinter import *
+import random
+class MyGui:
+    """
+    A GUI with buttons that change color and make the label grow
+    """
+    colors = ['blue', 'green', 'orange', 'red', 'brown', 'yellow']
+    def __init__(self, parent, title='popup'):
+        parent.title(title)
+        self.growing = False
+        self.fontsize = 10
+        self.lab = Label(parent, text='Gui1', fg='white', bg='navy')
+        self.lab.pack(expand=YES, fill=BOTH)
+        Button(parent, text='Spam', command=self.reply).pack(side=LEFT)
+        Button(parent, text='Grow', command=self.grow).pack(side=LEFT)
+        Button(parent, text='Stop', command=self.stop).pack(side=LEFT)
+    def reply(self):
+        "change the button's color at random on Spam presses"
+        self.fontsize += 5
+        color = random.choice(self.colors)
+        self.lab.config(bg=color, font=('courier', self.fontsize, 'bold italic'))
+    def grow(self):
+        "start making the label grow on Grow presses"
+        self.growing = True
+        self.grower()
+    def grower(self):
+        if self.growing:
+            self.fontsize += 5
+            self.lab.config(font=('courier', self.fontsize, 'bold'))
+            self.lab.after(500, self.grower)
+    def stop(self):
+        "stop the button growing on Stop presses"
+        self.growing = False
+class MySubGui(MyGui):
+    colors = ['black', 'purple']
+MyGui(Tk(), 'main')
+MyGui(Toplevel())
+MySubGui(Toplevel())
+mainloop()
